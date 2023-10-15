874da008fd1093ce25a29ed5f42bd40053fe00a9
viamill <126583359+viamill@users.noreply.github.com>
2023-10-15 15:19:58 -0400

    launcher

---
+++ trail.gd
@@ -0,0 +1,17 @@
+extends StaticBody2D
+
+@onready var launched : bool = false
+@onready var first : bool = false
+@onready var speed : float = 15
+@onready var direction
+@onready var angle = 0
+
+func _process(delta):
+	if launched:
+		if !first:
+			direction = (get_global_mouse_position()-global_position).normalized()
+			direction = direction.rotated(angle)
+		first = true
+		global_position += (direction * speed)
+		speed *= 0.96
+		if speed < 0.5: launched = false
